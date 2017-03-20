022c74f2aee0d051726a6211710718b971094021
Michel Bottan <michel.bottan@gmail.com>
2017-03-20 10:48:40 -0300

    External configuration variables

---
+++ src/config/environment.dev-template.ts
@@ -0,0 +1,10 @@
+export const ENV = {
+  PRODUCTION : false,
+  FIREBASE: {
+    apiKey: "",
+    authDomain: "<project-id>.firebaseapp.com",
+    databaseURL: "https://<project-id>.firebaseio.com",
+    storageBucket: "<project-id>.appspot.com",
+    messagingSenderId: ""
+  }
+};
